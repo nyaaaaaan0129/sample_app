15acc249ef943edbce549874dbdf4c49df19b68d
村田花穂 <muratahanaho@muratakasuinoMacBook-Air.local>
2020-04-26 15:44:18 +0900

    Finish 3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'cb648d8967593fa670e135fab451129a3481141c2472ea162defbe0dfe4eb4a74be0d47e65b8867be0511c8af2923c1d65a857215c32f7d2243439c6ebb4b39b'
